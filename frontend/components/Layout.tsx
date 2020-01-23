19432bef099ee4319f1f3f9a0c74db470a70f86b
scroobius-pip <sim04ful@gmail.com>
2020-01-23 17:26:18 +0100

    updated layout added footer

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,18 +1,26 @@
 import { colors } from '../styles'
 import NavBar from './NavBar'
+import Footer from './Footer'
 
 export default ({ children, noNav = false, userName = '' }) => {
     return <>
-        <div style={{ maxWidth: 800, margin: 'auto' }}>
+        <div style={{ margin: 'auto', minHeight: '100%', position: 'relative' }}>
             {!noNav && <NavBar userName={userName} />}
             <style jsx global>
                 {`
     body {
-        background: ${colors.background}
+        background: ${colors.background};
+        margin:0;
+   padding:0;
+   height:100%;
     }
     `}
             </style>
-            {children}
+            <div style={{ paddingBottom: 100 }}>
+
+                {children}
+            </div>
+            <Footer />
         </div>
     </>
 }
--- frontend/styles.ts
+++ frontend/styles.ts
@@ -1,7 +1,7 @@
 const colors = {
     primary: '#00A1FC',
     background: '#EFF3F5',
-    grey: '#565656'
+    grey: '#2b2e32'
 }
 
 export { colors }
